b46580e23f7eaf6eb8cc8222424706d38831bc63
Paul Aston <odders17@gmail.com>
2021-10-17 21:18:07 +0100

    added db js and created request

---
+++ public/db.js
@@ -0,0 +1,20 @@
+let db;
+let budgetVersion;
+
+//For budget database create new db request
+const request = indexedDB.open('budget', budgetVersion || 1);
+
+request.onupgradeneeded = function (e) {
+    console.log('Upgrade needed in IndexDB');
+
+    const { oldVersion } = e;
+    const newVersion = e.newVersion || db.version;
+
+    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+    db = e.target.result;
+
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('transactions', {autoIncrement: true});
+    }
+};
