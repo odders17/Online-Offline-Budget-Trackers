392c7d560402607ac45409e43592c574d18822b6
Paul Aston <odders17@gmail.com>
2021-10-23 19:17:33 +0100

    changed port back to 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
